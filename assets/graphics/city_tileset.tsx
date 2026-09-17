fdba8c47204ad46198bef22e0e7f0fa6255514a9
SebastianJRamirezA <sebastian.jose.ramirez.a@gmail.com>
2026-09-17 13:10:05 -0400

    Remove XML tilemaps and tilesets

--- assets/graphics/city_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2-2-geb83ddb9" name="city_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
- <image source="city_tileset.png" width="628" height="475"/>
-</tileset>
